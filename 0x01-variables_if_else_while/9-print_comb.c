02f5552c84b3270ea191ef6a80172290a8db4ff2
Theauthenticvic23 <victoradesina345@gmail.com>
2023-09-08 09:51:03 -0700

    print comb

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,12 +12,13 @@ int main(void)
 
 	for (i = 0 ; i < 10 ; i++)
 	{
-		if (i == 9)
+		if (i ==9)
 			putchar(i + '0');
 		else
-		{       putchar(i + '0');
-		        putchar(',');
-		        putchar(' ');
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	return (0);
