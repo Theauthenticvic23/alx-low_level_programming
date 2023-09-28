288702e7a262ad93e5d0421da8b2e6a183850f41
Theauthenticvic23 <victoradesina345@gmail.com>
2023-09-28 05:18:48 -0700

    reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print string in reverse
+ * @s: string to be reversed
+ *
+ * Return: void
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	s++;
+	_print_rev_recursion(s);
+	s--;
+	_putchar(*s);
+}
