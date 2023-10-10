2881fa0795287c0a278a8a86d6bad5e6a3dcadb8
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-10 04:10:43 -0700

    Woof

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: owner of dog
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
