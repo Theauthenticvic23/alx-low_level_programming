2c8e1969bb5b28a75147c7c1e4f10d5196b8bf1b
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-11 12:18:24 -0700

    3-main.c 3-op_functions.c 3-get_op_func.c 3-calc.h

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,69 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - add
+ * @a: args
+ * @b: args
+ *
+ * Return: nothing
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - subtraction
+ * @a: args
+ * @b: args
+ *
+ * Return: nothing
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - multiplication
+ * @a: args
+ * @b: args
+ *
+ * Return: nothing
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - division
+ * @a: args
+ * @b: args
+ *
+ * Return: nothing
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - modulus
+ * @a: args
+ * @b: args
+ *
+ * Return: nothing
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
+	return (a % b);
+}
