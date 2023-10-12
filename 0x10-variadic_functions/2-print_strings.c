01c6f832b3c0fc9e7e739879207213fa0025cedc
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-12 04:49:16 -0700

    Django

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -20,7 +20,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	{
 		str = va_arg(list, char *);
 		if (!str)
-			str = "nil";
+			str = "(nil)";
 		if (!separator)
 			printf("%s", str);
 		else if (separator && j == 0)
