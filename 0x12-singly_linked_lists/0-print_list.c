5b7669b3bdd2e5bea8a48ade24e4c84ea6bca155
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-19 04:55:30 -0700

    print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - school students
+ * @h: list
+ * Return: nothing
+ */
+size_t print_list(const list_t *h)
+{
+	int jump = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		jump++;
+		h = h->next;
+	}
+	return (jump);
+}
