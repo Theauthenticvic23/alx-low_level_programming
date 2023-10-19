29b5acc7beccc726c322921ba6e3d4b0bef621e2
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-19 13:59:46 -0700

    tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
-
 /**
- * first - prints a sentence before the main
- * function is executed
+ * bmain - main
+ * Return: nothing
  */
-void firat(void)
+
+void __attribute__ ((constructor)) bmain()
 {
-	printf("You're beat! and yet, you must allow, \n");
-	printf("I bore my house upon my back!\n");
+	printf("You're the beat! and yet, you must allow");
+	printf(", \nI bore my house upon my back!\n");
 }
