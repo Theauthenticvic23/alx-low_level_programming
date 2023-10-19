f90f47ef8bc42a48af75866b3cfd4aded34853c2
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-19 08:54:12 -0700

    hello

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,9 @@
-external printf; the C functiom, is to be called
-
-
-	section.data; Data section, initialized variables
-msg: 	db "Hello, Holberton", 0; C string needs 0
-fmt: 	db "%s", 10, 0; The printf format, "\n", '0'
-
-
-	section.text = Code section.
-
-
-    global main; the standard gcc entry point
-    main: ; the  program label for the entry point
-    push     rbp	: set up stack frame. must be aligned
-
-    mov rdi,fmt
-    mov rsi,msg
-    mov rax, 0; or can be xor rax, rax
-    call printf:  call C functiom
-
-
-   pop rbp; restore stack
-
-
-   mov rax, 0; normal, no error, return value
-   ret: return
+global	main
+	extern	printf
+main:
+	mov  edi, format
+	xor  eax, eax
+	call printf
+	mov  eax, 0
+	ret
+format: db 'Hello, Holberton\n',0	
