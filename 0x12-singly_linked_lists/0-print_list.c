3f2d64649abca1a9e8d1ce0ef0e45342f11e32f1
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-19 05:42:31 -0700

    print

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -17,11 +17,11 @@ size_t print_list(const list_t *h)
 	{
 		if (!h->str)
 		{
-			printf("[0] (nil)\n");
+			printf(" [0] (nil)\n");
 		}
 		else
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			printf(" [%d] %s\n", h->len, h->str);
 		}
 		jump++;
 		h = h->next;
