d15ee6befc62d3b0fe65fa0d672eddc85a8d7e95
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-19 17:34:12 -0700

    beat

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * bmain - function
- * Return: no return
+ * bmain - executed function
+ * Return: no return.
  */
+
 void __attribute__((constructor)) bmain()
 {
-	printf("You're the beat! and yet, you must allow");
+	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back!\n");
 }
