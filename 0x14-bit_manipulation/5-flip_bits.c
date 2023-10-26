f4415c72622871a016d66d2433ab5882ba9ce0e2
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-26 05:53:11 -0700

    flip bits

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * flip_bits - flip bits
+ * @n: number
+ * @m: second
+ * Return: nothing
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int a, countbit = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (a = 63; a >= 0; a--)
+	{
+		current = exclusive >> a;
+		if (current & 1)
+			countbit++;
+	}
+	return (countbit);
+}
