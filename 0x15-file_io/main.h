5af3958e350aa0fe9c129a50f2896f7b72917b66
Theauthenticvic23 <victoradesina345@gmail.com>
2023-10-31 00:53:13 -0700

    read text file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -10,5 +10,4 @@ int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 int _putchar(char c);
 int main(int argc, char *argv[]);
-void check_IO_stat(int stat, int fd, char *filename; char mode);
 #endif
